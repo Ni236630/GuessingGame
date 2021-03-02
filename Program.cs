05745ceecb79cb2f31392400dbf1da5b1bed9ba0
nicholas douso <nicholas.douso@gmail.com>
2021-03-02 09:48:19 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    Console.Write("WELCOME..To...The...Guessing Game!!!!");
+    Console.WriteLine(@"
+    .
+    ..
+    ...
+    ....
+    ...
+    ..
+    .");
+    Console.Write("To play simply type a number when told to.");
+    Console.WriteLine(@"
+    .
+    ..
+    ...
+    ....
+    ...
+    ..
+    .");
+    //Ask for & display user's guess 
+    UserGuess();   
+}
+
+void UserGuess(){
+    Console.WriteLine("Please enter a number:");
+    string UserGuessInput = Console.ReadLine();
+    Console.WriteLine($"Your guess was: {UserGuessInput}");
+}
